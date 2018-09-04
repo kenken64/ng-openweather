9be3a204abea00bc837e15fb8239c664a7476070
Kenneth Phang <bunnyppl@gmail.com>
2018-09-04 10:02:46 +0800

    updated app com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { OpenweatherServiceService } from './services/openweather-service.servic
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   /*
   (F-32)*5/9 = Celcius
   */
